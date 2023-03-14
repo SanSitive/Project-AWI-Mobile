c7cc4f5491f686b5f37499b6b7f450a906b099b5
Maxime Stefani <max341@free.fr>
2023-03-14 10:43:39 +0100

    First commit => init

---
+++ Projet Mobile/Projet_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Projet_MobileApp.swift
+//  Projet Mobile
+//
+//  Created by etud on 14/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Projet_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Projet MobileUITests/Projet_MobileUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Projet_MobileUITestsLaunchTests.swift
+//  Projet MobileUITests
+//
+//  Created by etud on 14/03/2023.
+//
+
+import XCTest
+
+final class Projet_MobileUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
